3a02e99d436d937bd74b7cb4705a8a416aa368cc
Dmitry Habibullin <d.khabibullin@dterra.eu>
2022-07-15 15:45:13 +0300

    add node express ejs

--- node-js-webDev/app.js
+++ node-js-webDev/app.js
@@ -40,19 +40,76 @@ readStream
 
 /** Создание сервера **/
 
+// const http = require('http')
+//
+// const PORT = 3000
+//
+// const server = http.createServer((req, res) => {
+//     console.log('Server request')
+//     console.log(req.url, req.method)
+//
+//     res.setHeader('Content-type', 'text/html')
+//
+//     res.write('<head><link rel="stylesheet" href="styles.css"></head>')
+//
+//     res.write('<h1>Hello Dimasic!</h1>')
+//     res.write('<p>Perviy paragraph!</p>')
+//     res.write('<p>Vtoroy paragraph!</p>')
+//     res.end()
+// })
+//
+// server.listen(PORT, 'localhost', (error) => {
+//     error ? console.log(error) : console.log(`listening port ${PORT}`)
+// })
+
+// ####
+// Возврат JSON
+// ####
+
+
 const http = require('http')
 
 const PORT = 3000
 
 const server = http.createServer((req, res) => {
     console.log('Server request')
+    console.log(req.url, req.method)
+
+    res.setHeader('Content-Type', 'application/json')
+
+    const data = JSON.stringify([
+        {name: 'Tommy', age: 35},
+        {name: 'KakoitoChuvak', age: 40}
+    ])
+
+    res.end(data)
 })
 
 server.listen(PORT, 'localhost', (error) => {
     error ? console.log(error) : console.log(`listening port ${PORT}`)
 })
 
+// ####
 
-
-
-
+// const http2 = require('http'),
+//     fs2 = require('fs'),
+//     PORT2 = 3000
+//
+// fs2.readFile('./index.html', (err, html) => {
+//     if (err) {
+//         throw err
+//     }
+//
+//     const server = http2.createServer((request, response) => {
+//         console.log('Server request')
+//         console.log(request.url, response.method)
+//
+//         response.setHeader('Content-type', 'text/html')
+//         response.write(html)
+//         response.end()
+//     })
+//
+//     server.listen(PORT2, 'localhost', (error) => {
+//         error ? console.log(error) : console.log(`listening port ${PORT2}`)
+//     })
+// })
