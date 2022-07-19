f093b9548081193cbdb0fc3176c5f047f6326600
Dmitry Habibullin <d.khabibullin@dterra.eu>
2022-07-19 18:23:31 +0300

    added ejs pages and logic

--- node-js-webDev/server.js
+++ node-js-webDev/server.js
@@ -3,20 +3,45 @@ const patch = require('path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+
 const PORT = 3000
 
-const createPath = (page) => patch.resolve(__dirname, 'views', `${page}.html`)
+const createPath = (page) => patch.resolve(__dirname, 'ejs-views', `${page}.ejs`)
 
 app.listen(PORT, (error) => {
     error ? console.log(error) : console.log(`Listening port ${PORT}`)
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'))
+    const title = 'Home'
+    res.render(createPath('index'), { title })
 })
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'))
+    const title = 'Contacts'
+    const contacts = [
+        { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+        { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
+        { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' }
+    ]
+
+    res.render(createPath('contacts'), { contacts, title })
+})
+
+app.get('/posts/:id', (req, res) => {
+    const title = 'Post'
+    res.render(createPath('post'), { title })
+})
+
+app.get('/posts', (req, res) => {
+    const title = 'Posts'
+    res.render(createPath('posts'), { title })
+})
+
+app.get('/add-post', (req, res) => {
+    const title = 'Add post'
+    res.render(createPath('add-post'), { title })
 })
 
 app.get('/about-us', (req, res) => {
@@ -24,8 +49,8 @@ app.get('/about-us', (req, res) => {
 })
 
 app.use((req, res) => {
-    // res.statusCode = 404
+    const title = 'Error page'
     res
         .status(404)
-        .sendFile(createPath('error'))
+        .render(createPath('error'), { title })
 })
